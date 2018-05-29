ec005e4dcb9cf3bf811d47741d9e86248d4bb887
Tom VanAntwerp <tom@tomvanantwerp.com>
2018-05-29 15:03:06 -0400

    Strip out tons of cruft

--- setup.sh
+++ setup.sh
@@ -1,54 +1,31 @@
 #!/usr/bin/env bash
 
 brews=(
-  autoenv
   aws-shell
   bash
   clib
-  dfc
   git
   git-extras
-  iftop
   imagemagick
-  lighttpd
-  lnav
-  mackup
   macvim
-  mtr
   ncdu
-  nmap
   node
-  postgresql
-  pgcli
-  scala
-  sbt
   stormssh
-  thefuck
   tmux
   tree
-  trash
   yarn
   wget
 )
 
 casks=(
-  adobe-reader
   alfred
   android-platform-tools
-  atom
-  betterzipql
-  cakebrew
-  cleanmymac
-  commander-one
   cyberduck
-  docker-toolbox
+  docker
   dropbox
   firefox
   google-chrome
   gitkraken
-  hosts
-  handbrake
-  istat-menus
   licecap
   iterm2
   qlcolorcode
@@ -61,33 +38,7 @@ casks=(
   sublime-text
   visual-studio-code
   vlc
-  volumemixer
   xquartz
-)
-
-pips=(
-  pylint
-  virtualenv
-  virtualenvwrapper
-)
-
-gems=(
-  bundle
-  github-pages
-  scss_lint
-)
-
-npms=(
-  eslint
-  gulp
-  vtop
-)
-
-clibs=(
-  bpkg/bpkg
-)
-
-bkpgs=(
 )
 
 git_configs=(
@@ -105,24 +56,8 @@ git_configs=(
   "user.email tom@tomvanantwerp.com"
 )
 
-apms=(
-  atom-beautify
-  file-icons
-  language-pug
-  linter-erb
-  linter-jscs
-  linter-jshint
-  linter-jsonlint
-  linter-htmlhint
-  linter-markdown
-  linter-php
-  linter-pug
-  linter-pylint
-  linter-scss-lint
-  linter-twig
-  minimap
-  nuclide
-  pigments
+code_extensions=(
+
 )
 
 fonts=(
@@ -186,31 +121,11 @@ echo 'eval "$(pyenv init -)"' >> ~/.zshrc
 source ${HOME}/.pyenv
 pyenv install 3.6.5
 pyenv install 2.7.15
+pyenv global 3.6.5
 
 echo "Installing ruby ..."
-brew install ruby-install chruby
-ruby-install ruby
-echo 'source /usr/local/share/chruby/chruby.sh' >> ~/.bash_profile
-echo 'source /usr/local/share/chruby/chruby.sh' >> ~/.zshrc
-chruby ruby-2.5.1
+brew install ruby
 ruby -v
-
-# echo "Installing PHP..."
-# TODO make this work
-# curl -L -O https://github.com/phpbrew/phpbrew/raw/master/phpbrew
-# chmod +x phpbrew
-# sudo mv phpbrew /usr/bin/phpbrew
-# echo "[[ -e ~/.phpbrew/bashrc ]] && source ~/.phpbrew/bashrc" >> ~/.bashrc
-# echo "[[ -e ~/.phpbrew/bashrc ]] && source ~/.phpbrew/bashrc" >> ~/.bash_profile
-# echo "[[ -e ~/.phpbrew/bashrc ]] && source ~/.phpbrew/bashrc" >> ~/.zshrc
-# source ~/.phpbrew/bashrc
-# phpbrew --debug install --stdout 7.1 as 7.1-dev +default +intl
-# phpbrew switch 7.1-dev
-# phpbrew --debug ext install xdebug 2.4.0
-# phpbrew --debug ext install github:krakjoe/apcu
-# phpbrew --debug ext install github:php-memcached-dev/php-memcached php7 -- --disable-memcached-sasl
-# phpbrew --debug ext install github:phpredis/phpredis php7
-# phpbrew install 5.6 as 5.6-dev like 7.0-dev
 
 echo "Installing Java ..."
 brew cask install java
@@ -230,37 +145,14 @@ install 'brew cask install --appdir=/Applications' ${casks[@]}
 echo "Installing fonts..."
 install 'brew cask install' ${fonts[@]}
 
-echo "Installing python packages..."
-install 'pip install --upgrade' ${pips[@]}
-
-echo "Installing ruby gems..."
-install 'gem install' ${gems[@]}
-
-echo "Installing secondary packages..."
-install 'clib install' ${clibs[@]}
-install 'bpkg install' ${bpkgs[@]}
-
 echo "Installing node packages..."
 install 'yarn global add' ${npms[@]}
-
-# echo "Installing Atom packages..."
-# install 'apm install' ${apms[@]}
-
-echo "Upgrading bash ..."
-sudo bash -c "echo $(brew --prefix)/bin/bash >> /private/etc/shells"
-cd; curl -#L https://github.com/barryclark/bashstrap/tarball/master | tar -xzv --strip-components 1 --exclude={README.md,screenshot.png}
-source ~/.bash_profile
 
 echo "Setting git defaults ..."
 for config in "${git_configs[@]}"
 do
   git config --global ${config}
 done
-
-echo "Upgrading ..."
-pip install --upgrade setuptools
-pip install --upgrade pip
-gem update --system
 
 echo "Cleaning up ..."
 brew cleanup
@@ -272,24 +164,7 @@ do
   echo "Failed to install: $fail"
 done
 
-echo "Run `mackup restore` after DropBox has done syncing"
-
 read -p "Hit enter to run [OSX for Hackers] script..." c
 sh -c "$(curl -sL https://gist.githubusercontent.com/brandonb927/3195465/raw/osx-for-hackers.sh)"
-
-#echo "Setting up fish shell ..."
-#brew install fish chruby-fish
-#echo $(which fish) | sudo tee -a /etc/shells
-#mkdir -p ~/.config/fish/
-#echo "source /usr/local/share/chruby/chruby.fish" >> ~/.config/fish/config.fish
-#echo "source /usr/local/share/chruby/auto.fish" >> ~/.config/fish/config.fish
-#echo "export GOPATH=/usr/libs/go" >> ~/.config/fish/config.fish
-#echo "export PATH=$PATH:$GOPATH/bin" >> ~/.config/fish/config.fish
-#chsh -s $(which fish)
-#curl -L https://github.com/oh-my-fish/oh-my-fish/raw/master/bin/install | fish
-#for omf in ${omfs[@]}
-#do
-#  fish -c "omf install ${omf}"
-#done
 
 echo "Done!"
